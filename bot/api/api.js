80d72b106b13b67456bea629bb673b324d8c9438
joelmakescode <joel.ros@web.de>
2026-03-25 19:46:15 +0100

    changed api in bot

--- bot/api/api.js
+++ bot/api/api.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const isLocal = process.env.NODE_ENV === 'local';
-
-const API_URL = isLocal ? 'http://localhost/api' : process.env.API_URL;
+const API_URL = process.env.API_URL;
 const API_TOKEN = process.env.API_TOKEN;
 
 const api = axios.create({
