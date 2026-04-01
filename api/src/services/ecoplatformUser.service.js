7903f7dcdfc497464a225a7e6d7aab4a5c266962
joelmakescode <joel.ros@web.de>
2026-04-01 19:59:36 +0200

    added back up code to ecoplatformuser

--- api/src/services/ecoplatformUser.service.js
+++ api/src/services/ecoplatformUser.service.js
@@ -3,13 +3,18 @@ const {hashPassword} = require("./handler/passwordhash.handler");
 
 async function insertEcoplatformUser(username, password) {
     const passwordHash = await hashPassword(password);
+    const backUpCode = generateBackupCode();
 
-    const sql = `INSERT IGNORE INTO ecoplatform_user_config (username, password_hash) VALUES (?, ?)`;
-    const params = [username, passwordHash];
+    const sql = `INSERT IGNORE INTO ecoplatform_user_config (username, password_hash, backup_code) VALUES (?, ?)`;
+    const params = [username, passwordHash, backUpCode];
 
     const [result] = await pool.execute(sql, params);
 
-    return result.affectedRows > 0 ? { info: result.info, username } : null;
+    return result.affectedRows > 0 ? { info: result.info, username, backUpCode } : null;
+}
+
+function generateBackupCode() {
+    return Math.floor(10000000 + 90000000 * Math.random());
 }
 
 async function selectEcoplatformUser(username) {
